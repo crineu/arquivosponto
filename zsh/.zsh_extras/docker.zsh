bf0c1a7b590d499a17305205d6dec7be08469ade
Crineu Tres <crineu.tres@ufsc.br>
2024-05-28 11:09:02 -0300

    Removido alias `lazydocker`
    Preferível instalação nativa ou via asdf/go
    (lembrar de `asdf reshim golang`)

--- zsh/.zsh_extras/docker.zsh
+++ zsh/.zsh_extras/docker.zsh
@@ -6,13 +6,6 @@ alias dkvl='docker volume ls'
 
 alias ttt='echo ~'
 
-# running lazydocker
-alias lazydocker='docker \
-  run --rm -it \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /~/.config/lazydocker:/.config/jesseduffield/lazydocker \
-  lazyteam/lazydocker'
-
 # running lazydocker portainer hml
 alias portainer-hml='docker \
   --host tcp://150.162.1.72:2376 --tls \
