e82bc93b6ac69364ea8683217686e3ef2a2ceab9
Crineu Tres <crineu.tres@ufsc.br>
2024-06-20 15:53:22 -0300

    Wezterm tamanho fullHD

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,9 +1,8 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
-
 
 -- This is where you actually apply your config choices
 
@@ -12,50 +11,49 @@ local config = wezterm.config_builder()
 -- config.color_scheme = 'GruvboxDark'
 -- config.color_scheme = 'Catppuccin Frappe'
 -- config.color_scheme = 'nord'
-config.color_scheme = 'Kanagawa (Gogh)'
+config.color_scheme = "Kanagawa (Gogh)"
 -- config.color_scheme = 'rose-pine'
 
-config.initial_cols = 162
-config.initial_rows = 42
+config.initial_cols = 152
+config.initial_rows = 38
 
 config.font_size = 13
 wezterm.font_with_fallback({
-  -- <built-in>, BuiltIn
-  "JetBrains Mono",
+	-- <built-in>, BuiltIn
+	"JetBrains Mono",
 
-  -- <built-in>, BuiltIn
-  -- Assumed to have Emoji Presentation
-  "Noto Color Emoji",
+	-- <built-in>, BuiltIn
+	-- Assumed to have Emoji Presentation
+	"Noto Color Emoji",
 
-  -- <built-in>, BuiltIn
-  "Symbols Nerd Font Mono",
-
+	-- <built-in>, BuiltIn
+	"Symbols Nerd Font Mono",
 })
 
 config.tab_bar_at_bottom = true
 config.keys = {
-  {
-    key = 'LeftArrow',
-    mods = 'SUPER',
-    action = wezterm.action.ActivateTabRelative(-1),
-  },
-  {
-    key = 'RightArrow',
-    mods = 'SUPER',
-    action = wezterm.action.ActivateTabRelative(1),
-  },
-  {
-    key = ',',
-    mods = 'SUPER',
-    action = wezterm.action.PromptInputLine {
-      description = 'New name for tab',
-      action = wezterm.action_callback(function(window, pane, line)
-        if line then
-          window:active_tab():set_title(line)
-        end
-      end),
-    }
-  },
+	{
+		key = "LeftArrow",
+		mods = "SUPER",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		key = "RightArrow",
+		mods = "SUPER",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = ",",
+		mods = "SUPER",
+		action = wezterm.action.PromptInputLine({
+			description = "New name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 }
 
 -- and finally, return the configuration to wezterm
