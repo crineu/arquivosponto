bf31103d94e298aeac5195f045552329f84bbe01
Crineu Tres <crineu.tres@ufsc.br>
2026-03-01 21:17:27 -0300

    up: tema Wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,12 +7,12 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Frappe"
+-- config.color_scheme = 'nord'
+-- config.color_scheme = "Kanagawa (Gogh)"
+-- config.color_scheme = 'rose-pine'
 -- config.color_scheme = 'Tokyo Night'
 -- config.color_scheme = 'GruvboxDark'
--- config.color_scheme = 'Catppuccin Frappe'
--- config.color_scheme = 'nord'
-config.color_scheme = "Kanagawa (Gogh)"
--- config.color_scheme = 'rose-pine'
 
 config.initial_cols = 152
 config.initial_rows = 38
